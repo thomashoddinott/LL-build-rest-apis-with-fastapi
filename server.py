1f1b75919107a7dc6c6d37daacbaae71e082740d
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-23 10:12:59 +0200

    sync and async handers

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
-from fastapi import FastAPI 
+import asyncio
+from time import sleep
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/health")
-def health():
-    return {"errors": None}
+@app.get('/sleep/sys')
+def nsys_sleep():
+    sleep(1)
+    # return {'error': None}
+    return {'obj': None}
+
+@app.get('/sleep/async-sys')
+# Don't do this
+async def sys_sleep():
+    await asyncio.sleep(1)
+    return {'error': None}
+
+@app.get('/sleep/async-aio')
+# test with `hey -c 10 -n 10 http://localhost:8000/sleep/sys`
+async def aio_sleep():
+    await asyncio.sleep(1)
+    return {'error': None}
 
 if __name__ == "__main__": #just run with `python server.py`
     import uvicorn
