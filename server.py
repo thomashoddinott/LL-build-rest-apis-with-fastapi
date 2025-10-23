e09e8d4cb173e9a0e38da473a10a3bc5c769376c
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-23 09:45:19 +0200

    just run with python server.py

--- server.py
+++ server.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"errors": None}
+
+if __name__ == "__main__": #just run with `python server.py`
+    import uvicorn
+    uvicorn.run(app)
