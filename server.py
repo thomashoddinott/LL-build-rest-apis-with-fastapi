7185dfef485df0250f7c6968790f10bd2b53d123
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-25 16:26:34 +0200

    ch5 challenge setup

--- server.py
+++ server.py
@@ -37,8 +37,19 @@ logging.basicConfig(
 )
 # endregion config_logging
 
+
 # region setup
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global _db
+    _db = sqlite3.connect("other_imports_ch05_04/trades.db", check_same_thread=False)
+    try:
+        yield
+    finally:
+        _db.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":  # production `python server.py`
     from argparse import ArgumentParser
@@ -58,23 +69,96 @@ if __name__ == "__main__":  # production `python server.py`
     uvicorn.run(app, port=settings.port)
 # endregion setup
 
+# === SERVER CODE BELOW ===
+
+# region ch05_challenge
+"""Make this server production ready:
+- Plug security holes
+- Add some logging
+- Add some metrics
+"""
+
+import math
+
+lat_km = 92
+lng_km = 111
+
+
+def distance(lat1, lng1, lat2, lng2):
+    """Return euclidean distance (in kilometers) between two coordinates.
+
+    >>> distance(0, 0, 1, 1)
+    144.1700384962146
+    """
+    delta_lat = (lat1 - lat2) * lat_km
+    delta_lng = (lng1 - lng2) * lng_km
+    return math.hypot(delta_lat, delta_lng)
+
+
+def parse_csv(fp):
+    print(fp)
+    """Parse CSV, returns tuple of:
+    - number of samples
+    - total distance
+    - average speed in km/h
+    """
+    reader = csv.DictReader(fp)
+    start_time = end_time = None
+    prev_lat = prev_lng = None
+    count = total_distance = 0
+
+    for row in reader:
+        count += 1
+        time = datetime.fromisoformat(row["time"])
+        # Rows are in chronological order
+        if start_time is None:
+            start_time = time
+        else:
+            end_time = time
+
+        lat, lng = float(row["lat"]), float(row["lng"])
+        if prev_lat:
+            total_distance += distance(lat, lng, prev_lat, prev_lng)
+        prev_lat, prev_lng = lat, lng
+
+    duration_hours = (end_time - start_time).total_seconds() / (60 * 60)
+    speed = total_distance / duration_hours
+
+    return count, total_distance, speed
+
+
+@app.post("/run")
+async def run_stats(request: Request):
+    data = await request.body()
+    fp = StringIO(data.decode())
+    count, distance, speed = parse_csv(fp)
+    out = {
+        "count": count,
+        "distance": distance,
+        "speed": speed,
+    }
+    return out
+
+
+# endregion ch05_challenge
+
 # region Databases
 # $ `cd other_imports_ch05_04 && bash create-db.sh`
 _db = None
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    global _db
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     global _db
 
-    _db = sqlite3.connect("other_imports_ch05_04/trades.db", check_same_thread=False)
-    try:
-        yield
-    finally:
-        _db.close()
+#     _db = sqlite3.connect("other_imports_ch05_04/trades.db", check_same_thread=False)
+#     try:
+#         yield
+#     finally:
+#         _db.close()
 
 
-app = FastAPI(lifespan=lifespan)
+# app = FastAPI(lifespan=lifespan)
 
 
 def get_cursor():
